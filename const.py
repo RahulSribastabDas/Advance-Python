c4e12d17e16de9f9c05377fb39e5d0687bab82b7
Rahul Sribastab Das <rahulsribastabdas.com>
2026-02-24 23:21:48 +0530

    constructor program

---
+++ const.py
@@ -0,0 +1,16 @@
+# constructor for set, tuple, list and dictionary
+s=set((1,2,4,3,897,98,786))
+print(s)
+print(type(s))
+
+d=dict(name="Rahul",roll_no=546,marks={90,56,41})
+print(d)
+print(type(d))
+
+t=(1,2,4,3,897,98,786)
+print(t)
+print(type(t))
+
+l=[1,2,4,3,897,98,786]
+print(l)
+print(type(l))
