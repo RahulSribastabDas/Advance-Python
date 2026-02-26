f49e5cb4e519de928e486d5495236b6626b443e0
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-02-26 10:51:03 +0530

    user input program

--- Oops/studentdetail.py
+++ Oops/studentdetail.py
@@ -16,3 +16,39 @@ section = input("Enter student's section: ")
 city = input("Enter student's city: ")
 student = Student(name, roll_number, section, city)
 student.display()
+
+#####2
+from demouserinput import Student
+
+class Student:
+    def __init__(self, name, age, branch):
+        self.name = name
+        self.age = age
+        self.branch = branch
+
+    # Make sure this is indented so it is INSIDE the class
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Branch: {self.branch}")
+
+s= Student("Alice", 20,"CSE")
+s.display()  
+
+#access variables outside the class
+print("access outside the class",s.name)
+print(s.age)
+
+#delete variable outside the class
+del s.name
+#print(s.name)
+
+#modify variable outside the class
+print("Before modification:", s.age)
+s.age = 40
+print("After modification:", s.age)
+
+#Add new variable in existing class
+s.city="Pune"
+print("City:", s.city)
+
