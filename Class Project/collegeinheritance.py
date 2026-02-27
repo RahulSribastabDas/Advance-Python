812e95c2e2df4c81bb83d47c0f750ae8754042fd
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-02-27 22:05:50 +0530

    Project 2

---
+++ Class Project/collegeinheritance.py
@@ -0,0 +1,42 @@
+# college is the main class and student has name, rollno, section
+class College:
+    def __init__(self, name):
+        self.name = name
+
+
+class Student(College):
+    def __init__(self, name, rollno, section):
+        self.name = name
+        self.rollno = rollno
+        self.section = section
+
+
+class AcademicMark(Student):
+    def __init__(self, name, rollno, section, marks):
+        
+        Student.__init__(self, name, rollno, section)
+        self.marks = marks
+
+
+class SportsMark(Student):
+    def __init__(self, name, rollno, section, sports_marks):
+        
+        Student.__init__(self, name, rollno, section)
+        self.sports_marks = sports_marks
+
+
+class StudentReport(AcademicMark, SportsMark):
+    def __init__(self, name, rollno, section, marks, sports_marks):
+        AcademicMark.__init__(self, name, rollno, section, marks)
+        SportsMark.__init__(self, name, rollno, section, sports_marks)
+
+    def display_report(self):
+        print(f"Student Name: {self.name}")
+        print(f"Roll No: {self.rollno}")
+        print(f"Section: {self.section}")
+        print(f"Academic Marks: {self.marks}")
+        print(f"Sports Marks: {self.sports_marks}")
+
+
+student_report = StudentReport("John Doe", "12345", "A", 85, 90)
+student_report.display_report()
