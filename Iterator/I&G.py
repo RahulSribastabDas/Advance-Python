e78ffcf7a86287d72ca8ac09dd89b4b5cbfffd36
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-03-11 12:00:00 +0530

    Your commit message

---
+++ Iterator/I&G.py
@@ -0,0 +1,32 @@
+"""
+l=[10,20,30,40]
+it=iter(l) 
+print(next(it))  #10   next print only one value at a time to save our space.
+print(next(it))  #20
+print(next(it))  #30
+print(next(it))  #40
+print(next(it))  #Error StopIteration
+
+"""
+'''
+#list
+l=[10,20,30,40]
+it=iter(l) 
+for i in it:
+    print(i)
+'''
+
+'''
+#Tuple
+l=(10,20,30,40)
+it=iter(l) 
+for i in it:
+    print(i)
+'''
+"""
+#String
+text="Ranjeet"
+it=iter(text)
+for i in it:
+    print(i)
+"""
