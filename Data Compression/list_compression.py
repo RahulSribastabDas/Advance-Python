37cd37bc1183658d759f1b23a18a82e2efdd7629
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-03-14 10:38:41 +0530

    Convert the lowercase to uppercase

--- Data Compression/list_compression.py
+++ Data Compression/list_compression.py
@@ -1,4 +1,10 @@
 #List Comprehension
-I=[1,2,3,4,5]
+'''I=[1,2,3,4,5]
 empty_list = [i*1 for i in I] #Syntax [expression for variable _name in iterable]
-print(empty_list)
+print(empty_list)'''
+
+#convert l=["ram","seeta","riya"] to l1=["RAM","SEETA","RIYA"]
+l=["ram","seeta","riya"]
+l1=[i.upper() for i in l]
+print(l1)
+
