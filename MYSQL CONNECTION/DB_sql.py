57fa84acd2a81aa0979059d0c66624b16835b170
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-03-24 10:15:31 +0530

    Mysql connection - create database, table,show tables,insert data into tables,multiple data ,read data inside table

--- MYSQL CONNECTION/DB_sql.py
+++ MYSQL CONNECTION/DB_sql.py
@@ -1,3 +1,39 @@
 import mysql.connector as my
-mydb=my.connect(host="localhost",user="root",password="Rahul@1002")
-print("Connection Created")
+mydb=my.connect(host="localhost",user="root",password="Rahul@1002",database="new_db")
+db_cursor=mydb.cursor()
+
+#step1: create connection
+#print("Connection Created")
+
+#Step2: create database  
+'''db_cursor.execute("create database new_db")
+print("Database Created")'''
+
+#create table
+''''db_cursor.execute("create table emp(id int,name varchar(20))")
+print("Table Created")'''
+
+#step 4: show tables
+'''db_cursor.execute("show tables")
+for i in db_cursor:
+    print(i)'''
+
+#step 5: insert data into table
+'''db_cursor.execute("insert into emp(id,name)values(%s,%s)",(1,"Rahul")) 
+print(db_cursor.rowcount,"data inserted")  '''
+
+#insert multiple data into table
+'''db_insert="insert into emp(id,name)values(%s,%s)"
+db_value=[(2,"Ashish"),(3,"Rajeev"),(4,"Riya"),(5,"Seeta")]
+db_cursor.executemany(db_insert,db_value)
+mydb.commit()
+print(db_cursor.rowcount,"data inserted")'''
+
+#step 6: read data inside table
+'''db_cursor.execute("select * from emp")
+db_select=db_cursor.fetchall()
+print(db_cursor)'''
+
+db_cursor.execute("select * from emp")
+for i in db_cursor.fetchall():
+    print(i)
