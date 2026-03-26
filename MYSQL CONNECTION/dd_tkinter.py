575897f500205b7b86eb5d65855e1f3cd1abc77f
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-03-26 11:07:08 +0530

    Module Tkinter we used Button,Label,Checkbox

--- MYSQL CONNECTION/dd_tkinter.py
+++ MYSQL CONNECTION/dd_tkinter.py
@@ -5,7 +5,30 @@
 from tkinter import *
 root=Tk() #used to create a appliaction using TK functions
 root.title("My First Application using tkinter")
-#root.geometry("400x300")
-root.minsize(width=100,height=300)
+root.geometry("400x300")
+#root.minsize(width=100,height=300)
 #root.maxsize(width=400,height=600)
+
+#1.Button
+'''def greet():
+    print("Welcome to Python Programming")
+    print("Hello, World!")
+
+btn =Button(root,text="click",font=("Arial",12),bg="Green",fg="Red",command=greet)
+btn.pack()'''
+
+#2.Label
+'''root.title("Label Example")
+l1 = Label(root,text="Welcome to Python Programming",font=("Arial",12),bg="Green",fg="Red")
+l1.place(x=100,y=50)
+l1.pack()'''
+
+#3.checkbox
+def check():
+    print(var.get())
+
+var=StringVar    
+chk=Checkbutton(root,text="Accept",variable=var,bg="Red")
+chk.pack()
+b=Button(root,command=check)
 root.mainloop()#
