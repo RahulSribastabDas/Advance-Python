5bf2993881c11b3aa6aae4acff10ffaeaf19b9c7
Rahul Sribastab Das <rahulsribastabdas@gmail.com>
2026-03-26 10:49:56 +0530

    Started the tkinter module created a gui application where i done geometry,minsize,maxsize

---
+++ MYSQL CONNECTION/dd_tkinter.py
@@ -0,0 +1,11 @@
+#GUI - Tkinter
+
+#widgets - button,Label,Checkbox,Radiobutton,ListBox
+
+from tkinter import *
+root=Tk() #used to create a appliaction using TK functions
+root.title("My First Application using tkinter")
+#root.geometry("400x300")
+root.minsize(width=100,height=300)
+#root.maxsize(width=400,height=600)
+root.mainloop()#
